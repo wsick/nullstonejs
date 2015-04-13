4953ab540809350924a2ceee9f8f5796defda596
BSick7 <brad.sickles@gmail.com>
2015-04-12 20:14:08 -0400

    Creating separate *.map files to reduce content in js files.

--- gulp/default.js
+++ gulp/default.js
@@ -1,9 +1,6 @@
 var gulp = require('gulp'),
     ts = require('gulp-typescript'),
     sourcemaps = require('gulp-sourcemaps'),
-    uglify = require('gulp-uglify'),
-    rename = require('gulp-rename'),
-    merge = require('merge2'),
     srcs = [
         'typings/*.d.ts',
         'src/_version.ts',
@@ -13,25 +10,17 @@ var gulp = require('gulp'),
 
 module.exports = function (meta) {
     var tsProject = ts.createProject({
-        declarationFiles: true,
         target: 'ES5',
         out: meta.name + '.js',
-        removeComments: true
+        removeComments: true,
+        sourceRoot: '../src'
     });
 
     gulp.task('default', function () {
-        var tsr = gulp.src(srcs)
+        return gulp.src(srcs)
             .pipe(sourcemaps.init())
-            .pipe(ts(tsProject));
-
-        return merge([
-            tsr.dts.pipe(gulp.dest('dist')),
-            tsr.js.pipe(sourcemaps.write())
-                .pipe(gulp.dest('dist'))
-                .pipe(uglify())
-                .pipe(sourcemaps.write())
-                .pipe(rename('nullstone.min.js'))
-                .pipe(gulp.dest('dist'))
-        ]);
+            .pipe(ts(tsProject))
+            .pipe(sourcemaps.write('./'))
+            .pipe(gulp.dest('dist'));
     });
 };
--- gulp/dist.js
+++ gulp/dist.js
@@ -1,7 +1,41 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    ts = require('gulp-typescript'),
+    sourcemaps = require('gulp-sourcemaps'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    merge = require('merge2'),
+    srcs = [
+        'typings/*.d.ts',
+        'src/_version.ts',
+        'src/*.ts',
+        'src/**/*.ts'
+    ];
 
 module.exports = function (meta) {
-    gulp.task('dist', ['bump', 'default']);
-    gulp.task('dist-minor', ['bump-minor', 'default']);
-    gulp.task('dist-major', ['bump-major', 'default']);
+    gulp.task('dist-build', function () {
+        var tsProject = ts.createProject({
+            declarationFiles: true,
+            target: 'ES5',
+            out: meta.name + '.js',
+            removeComments: true,
+            sourceRoot: '../src'
+        });
+
+        var tsr = gulp.src(srcs)
+            .pipe(sourcemaps.init())
+            .pipe(ts(tsProject));
+
+        return merge([
+            tsr.dts.pipe(gulp.dest('dist')),
+            tsr.js
+                .pipe(uglify())
+                .pipe(rename('nullstone.min.js'))
+                .pipe(sourcemaps.write('./'))
+                .pipe(gulp.dest('dist'))
+        ]);
+    });
+
+    gulp.task('dist', ['bump', 'dist-build']);
+    gulp.task('dist-minor', ['bump-minor', 'dist-build']);
+    gulp.task('dist-major', ['bump-major', 'dist-build']);
 };
