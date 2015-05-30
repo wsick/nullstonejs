fe39fb465cc54c036474eebf7f41740231b8d6c3
BSick7 <brad.sickles@gmail.com>
2015-05-30 19:45:19 -0400

    Fixing for tsc 1.5.

--- Gruntfile.js
+++ Gruntfile.js
@@ -99,7 +99,7 @@ module.exports = function (grunt) {
                 dest: '<%= dirs.test.build %>',
                 options: {
                     target: 'es5',
-                    basePath: '<%= dirs.test.root %>',
+                    rootDir: '<%= dirs.test.root %>',
                     module: 'amd',
                     sourceMap: true
                 }
@@ -114,7 +114,7 @@ module.exports = function (grunt) {
                 dest: '<%= dirs.stress.build %>',
                 options: {
                     target: 'es5',
-                    basePath: '<%= dirs.stress.root %>',
+                    rootDir: '<%= dirs.stress.root %>',
                     module: 'amd',
                     sourceMap: true
                 }
